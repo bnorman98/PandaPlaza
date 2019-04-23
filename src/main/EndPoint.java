ab60c5410922c2abbc45a15aba4c58a1f10a9a68
zoltan31 <nemethzoli1997@gmail.com>
2019-04-23 23:49:20 +0200

    Kommentezések

--- src/main/EndPoint.java
+++ src/main/EndPoint.java
@@ -3,32 +3,60 @@ package main;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+/**
+ * A kijáratot reprezentálja a játékban
+ */
 public class EndPoint extends Tile {
-	
+	/**
+	 * A bejárat, ahova az emelet elhagyásakor kerül az állat
+	 */
 	private Tile startPoint;
-	
+
+	/**
+	 * A kijárat konstruktora
+	 */
 	public EndPoint() {
 		startPoint = null;
 	}
+
+	/**
+	 * A kijárat konstruktora
+	 */
 	public EndPoint(Tile startPoint) {
 		this.startPoint = startPoint;
 	}
-	
+
+	/**
+	 * Visszaadja a bejáratot
+	 * @return
+	 */
 	public Tile getStartPoint() {
 		return startPoint;
 	}
-	
-	// Double check me please
+
+	/**
+	 * A kijárat lép egyet
+	 */
 	public void stepped() {
 		System.out.println("Endpoint.stepped called");
+		//Ha a kijáraton nincs állat, akkor nem történik semmi
 		if(animal.countFollowers() == 0)
 			return;
+		//Ellenkező esetben pontot adunk az adott állatnak
 		animal.addScore(animal.countFollowers());
+		//Elvesszük az állat összes követőjét
 		animal.killFollowers();
+		//Az állatot a bejáratra tesszük
 		animal.goTo(startPoint);
 	}
 
+	/**
+	 * Szerializálja magát a paraméterként megadott objektumba
+	 * @param pw Amibe szerializájuk az objektumot
+	 */
 	public void writeOut(PrintWriter pw){
+		//Println függvénnyel kiírjuk az osztály nevét,
+		//tagfüggvényeit és a tagfüggvények értékeit
 		pw.println("EndPoint");
 		pw.println("-ID: " + this.getID());
 		for (int i=0;i<this.getNumOfNeighbours();i++){
@@ -46,12 +74,21 @@ public class EndPoint extends Tile {
 		pw.println("-life: " + this.getLife());
 	}
 
+	/**
+	 * Deszerializálja az objektumot
+	 * @param lines Szövegsorok, amikből deszerializáljuk az objektumot
+	 * @param idx index értéke
+	 */
 	public void readIn(ArrayList<String> lines, int idx){
+		// Az indextől a szövegsorok végéig olvasunk
 		for (int i=idx+1;i<lines.size();i++){
+			//Space szerint kettéválasztjuk a stringeket
 			String[] parts = lines.get(i).split(" ");
+			//A '-' karaktert eltávolítjuk az elejéről
 			if (parts[0].charAt(0) != '-'){
 				i = lines.size();
 			}
+			//switch-casezel beállítjuk a tagváltozókat
 			switch (parts[0]){
 				case "-startID:":
 					this.startPoint = Game.getInstance().getTileContained(Integer.parseInt(parts[1]));
